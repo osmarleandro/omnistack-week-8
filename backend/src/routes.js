68f9555664bb504a1d0d24b71dbabb288451f776
Osmar Leandro <osmarleandro@ufcg.edu.br>
2019-08-07 15:38:29 -0300

    config devs controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const DevController = require('./controllers/DevController');
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => res.json({ message: `Hello ${req.query.name}` }));
-routes.post('/devs', (req, res) => {
-  res.json({ message: 'the devs' });
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
